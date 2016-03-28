6110e4ebd84487d66fbda2969c54d90a43cbd6a2
IIVIIO <i.kvashnev@gmail.com>
2016-03-28 13:00:22 +0300

    Работа над canvas

--- build/js/check.js
+++ build/js/check.js
@@ -1,16 +1,14 @@
 
 
 function getMessage(a, b) {
-  if (typeof a == 'boolean') 
-  {
-    if (a) {
-      return 'Переданное GIF-изображение анимировано и содержит ' + b + ' кадров';
-    } else {
-      return 'Переданное GIF-изображение не анимировано';
+  if (typeof a  ==  'boolean'){
+    if (a) { return 'Переданное GIF-изображение анимировано и содержит ' + b + ' кадров';
+      } else {
+        return 'Переданное GIF-изображение не анимировано';
     }
   }
 
- if (typeof a == 'number') {
+  if(typeof a == 'number') {
     return 'Переданное SVG-изображение содержит ' + a + ' объектов и ' + (b * 4) + ' аттрибутов';
 
   }
